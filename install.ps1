b5ee9e6fdf04923134e79aa2f74e0949d4d6718b
Christopher Anderson <chris@nullcode.org>
2020-01-04 13:35:12 -0800

    [windows] Add powershell script for setting up environment

---
+++ install.ps1
@@ -0,0 +1,31 @@
+﻿$files = ".bashrc",
+         ".bashrc.Msys",
+         ".bash_profile",
+         ".gitconfig",
+         ".git-prompt.sh",
+         ".minttyrc",
+         ".vim",
+         ".vimrc"
+
+function install {
+    ForEach ($file in $files) {
+        New-Item -Force -Path ~/$file -ItemType SymbolicLink -Value $PSScriptRoot/$file
+    }
+}
+
+function clean {
+    ForEach ($f in $files) {
+        $fp = "$HOME\$f"
+        if (Test-Path $fp) {
+            write-host removing $fp
+            (Get-Item $fp).Delete()
+        }
+    }
+}
+
+if ($args[0] -eq "-c") {
+    clean
+} else {
+    install
+}
+
